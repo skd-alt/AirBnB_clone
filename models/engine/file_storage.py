c34b268d8a87b6dd423e56263debd18e12488215
skd-alt <sipho@skanalytics.co.za>
2023-10-15 03:07:30 -0700

    Fix json storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,34 +3,35 @@
 
 import json
 import os
-from models.base_model import BaseModel
 
 
 class FileStorage:
     """Store and recreate objects"""
-    __file_path = "./storage_file.json"
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns all objects."""
-        return __objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key"""
         key_obj = "{}.{}".format(type(obj).__name__, obj.id)
-        __objects[key] = obj
+        FileStorage.__objects[key_obj] = obj
 
     def save(self):
         """serializes to json file"""
-        with open(__file_path, "w", encoding="utf-8") as f:
-            dicn = {k: v.to_dict() for (k, v) in __objects.items()}
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            dicn = {k: v.to_dict() for (k, v) in FileStorage.__objects.items()}
             json.dump(dicn, f)
 
     def reload(self):
         """deserializes the JSON file to __objects."""
-        if not os.path.isfile(__file_path):
+        from models.base_model import BaseModel
+
+        if not os.path.isfile(FileStorage.__file_path):
             return
 
-        with open(__file_path, "r", encoding="utf-8") as f:
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
             obj_dicn = json.load(f)
-            __objects = {k: BaseModel(**v) for (k, v) in obj_dicn.items}
+            FileStorage.__objects = {k: BaseModel(**v) for (k, v) in obj_dicn.items()}
